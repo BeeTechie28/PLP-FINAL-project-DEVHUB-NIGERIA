54ceb1b71ff1f502a9a5a3a4f9e4189e681e3a1d
BeeTechie28 <izikogbobula@gmail.com>
2025-11-22 04:44:36 +0100

    Delete frontend directory

--- frontend/src/components/index.js
+++
@@ -1,7 +0,0 @@
-export{default as Navbar} from ".//Navbar/Navbar";
-export {default as Hero} from ".//Hero/Hero";
-export {default as Footer} from ".//Footer/Footer";
-export { default as ProgramsOverview } from "./ProgramsOverview/ProgramsOverview";
-export{default as  CommunityHighlight} from "./CommunityHighlight/CommunityHighlight";
-export { default as AboutPreview } from "./AboutPreview/AboutPreview";
-export { default as ContactSection } from "./ContactSection/ContactSection";
