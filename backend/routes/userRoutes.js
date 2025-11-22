3b8cd3b0f08ff183f125560be52e668cb8fab394
BeeTechie28 <izikogbobula@gmail.com>
2025-11-22 16:49:45 +0100

    add backend

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const { createUser, getUsers, getUser, updateUser, deleteUser } = require('../controllers/userController');
+
+// Create a new user
+router.post('/', createUser);
+
+// Get all users
+router.get('/', getUsers);
+
+// Get a single user
+router.get('/:id', getUser);
+
+// Update a user
+router.put('/:id', updateUser);
+
+// Delete a user
+router.delete('/:id', deleteUser);
+
+module.exports = router;
